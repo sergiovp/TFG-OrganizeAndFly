aee497ca4a3be6d68dbed21a910c963f54c6dd85
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-14 21:12:18 +0200

    Implemented login route, advance #2

---
+++ app/backend/routes/routes.ts
@@ -0,0 +1,24 @@
+/**
+ * API routes file
+ */
+
+import express from 'express';
+import * as userController from '../user/userController';
+import * as userModel from '../user/userModel';
+import status from '../helpers/statusCodes'; 
+
+const router: express.Router = express.Router();
+
+/************************
+ * User's routes:
+ ***********************/
+
+router.post('/login', async function (req: express.Request, res: express.Response) {
+    const { email, pass } = req.body;
+
+    const response = await userController.logIn(email, pass);
+
+    res.status(response.status || status.Success).send(response.msg || response.token);
+});
+
+export default router;
