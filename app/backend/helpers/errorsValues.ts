21361b31c2d197a814b9379e21129778bda6cfdd
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-14 20:40:55 +0200

    Added errors values enum

---
+++ app/backend/helpers/errorsValues.ts
@@ -0,0 +1,8 @@
+export const enum signErrors {
+    DuplicatedEmail = "The entered email is already in use",
+    PasswordsDontMatch = "The entered passwords must match",
+    EmailBadSyntax = "The entered email is not valid",
+    BadArguments = "You must entered the required data",
+    UnsafePass = "The password must be at least 6 characters long",
+    WrongData = "The entered data is incorrect, please, try again",
+};
