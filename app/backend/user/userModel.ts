994b3548b2fe852cd5ec3c084ee9df259061a760
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-15 11:23:51 +0200

    Implemented signup db query function. Advance #1

--- app/backend/user/userModel.ts
+++ app/backend/user/userModel.ts
@@ -3,6 +3,19 @@ import { getBDError } from '../helpers/errorsFunctions';
 
 const TABLE_NAME = 'users';
 
+export async function signUpDB(userID: string, email:string, pass: string): Promise<any> {
+    try {
+        await dataBase(TABLE_NAME)
+            .insert({ user_id: userID, email: email, pass: pass })
+        return await dataBase(TABLE_NAME)
+            .select('user_id', 'email', 'pass')
+            .first()
+            .where({ user_id: userID });
+    } catch (err) {
+        return getBDError(err);
+    }
+}
+
 export async function logInDB(email: string, pass: string): Promise<any> {
     try {
         return await dataBase(TABLE_NAME)
