7ddcd5dc97994fa3fa52ad92643df36b83bd5e57
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-15 11:33:32 +0200

    Implemented some error functions to return an object with the error result

--- app/backend/helpers/errorsFunctions.ts
+++ app/backend/helpers/errorsFunctions.ts
@@ -1,4 +1,4 @@
-import { signErrors } from './errorsValues';
+import { signErrors, tokenErrors } from './errorsValues';
 import status from './statusCodes';
 
 export function getBadArguments(): object {
@@ -19,11 +19,65 @@ export function getWrongData(): object {
     };
 }
 
+export function getEmailBadSintax(): object {
+    return {
+        msg: {
+            error: { EmailBadSyntax: signErrors.EmailBadSyntax },
+        },
+        status: status.BadRequest,
+    };
+}
+
+export function getMatchPasswords() {
+    return {
+        msg: {
+            error: {PasswordsDontMatch: signErrors.PasswordsDontMatch },
+        },
+        status: status.BadRequest,
+    };
+}
+
+export function getSecurePass() {
+    return {
+        msg: {
+            error: {UnsafePass: signErrors.UnsafePass },
+        },
+        status: status.BadRequest,
+    };
+}
+
+export function getDuplicateEmail() {
+    return {
+        msg: {
+            error: {DuplicatedEmail: signErrors.DuplicatedEmail },
+        },
+        status: status.BadRequest,
+    };
+}
+
 export function getBDError(err: any): object {
     return {
         msg: {
             error: { BDError: err.message },
         },
         status: status.Notfound,
+    };
+}
+
+export function getTokenNotProvided() {
+    return {
+        msg: {
+            error: { TokenDoesNotProvided: tokenErrors.TokenDoesNotProvided },
+        },
+        status: status.Unauthorized,
+    };
+}
+
+export function getTokenNotMatch() {
+    return {
+        msg: {
+            error: { TokenDoesNotMatch: tokenErrors.TokenDoesNotMatch },
+        },
+        status: status.Unauthorized,
     };
 }
