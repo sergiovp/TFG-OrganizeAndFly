3857efd8660bdb731abae77797d0e27b96c355ef
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-15 13:20:49 +0200

    Implemented a function to return an ID using uuid

--- app/backend/helpers/generations.ts
+++ app/backend/helpers/generations.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
@@ -5,6 +6,10 @@ const SECRET = 'app_secret_key';
 
 export function generateUserToken(userData: string): string {
     return jwt.sign(userData, SECRET);
+}
+
+export function generateID(): string {
+    return uuidv4();
 }
 
 export async function generateEncryptedPass(pass: string, saltRound: number = 10): Promise<string> {
