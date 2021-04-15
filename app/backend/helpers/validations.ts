7c6f6790e524a32e61d85e7360e09acf84061c61
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-15 11:34:17 +0200

    Added token errors values

--- app/backend/helpers/validations.ts
+++ app/backend/helpers/validations.ts
@@ -3,3 +3,12 @@ import bcrypt from 'bcrypt';
 export async function validateEncryptedPass(introducedPass: string, DBPass: string): Promise<boolean> {
     return await bcrypt.compare(introducedPass, DBPass);
 }
+
+export function validateEmail(email: string): boolean {
+    const re: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export function validateSecurePass(pass: string): boolean {
+    return pass.length >= 6 ? true : false;
+}
