0f5ba7f873cb36fd61bb0da1f63113802f7d4ef6
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-15 13:20:09 +0200

    Added a function to generate an excrypted password

--- app/backend/helpers/generations.ts
+++ app/backend/helpers/generations.ts
@@ -1,7 +1,12 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const SECRET = 'app_secret_key';
 
 export function generateUserToken(userData: string): string {
     return jwt.sign(userData, SECRET);
+}
+
+export async function generateEncryptedPass(pass: string, saltRound: number = 10): Promise<string> {
+    return await bcrypt.hash(pass, saltRound);
 }
