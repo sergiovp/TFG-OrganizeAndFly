bce879a41cb50a369da72fa4d23226ad4e121a28
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-15 13:27:25 +0200

    Implemented signun user controller function. Advance #1

--- app/backend/user/userController.ts
+++ app/backend/user/userController.ts
@@ -3,6 +3,39 @@ import * as errorFunctions from '../helpers/errorsFunctions';
 import * as validations from '../helpers/validations';
 import * as generations from '../helpers/generations';
 
+export async function signUp(email: string, pass: string, passConfirmation: string): Promise<any> {
+    if (!email || !pass || !passConfirmation) {
+        return errorFunctions.getBadArguments();
+    }
+
+    if (!(validations.validateEmail(email))) {
+        return errorFunctions.getEmailBadSintax();
+    }
+
+    if (!(validations.validateSecurePass(pass))) {
+        return errorFunctions.getSecurePass();
+    }
+
+    if (pass !== passConfirmation) {
+        return errorFunctions.getMatchPasswords();
+    }
+
+    const userID = generations.generateID();
+    const encryptedPass = await generations.generateEncryptedPass(pass);
+
+    const userData = await userModel.signUpDB(userID, email, encryptedPass);
+
+    if (userData.error) {
+        if (validations.validateDuplicateEmail(userData.error)) {
+            return errorFunctions.getDuplicateEmail();
+        }
+        return userData;
+    }
+    const token = generations.generateUserToken(userData);
+
+    return {token: token};
+}
+
 export async function logIn(email: string, pass: string): Promise<any> {
     if (!email || !pass) {
         return errorFunctions.getBadArguments();
