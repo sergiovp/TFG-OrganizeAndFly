1b9884d9f7a6a8620a11029de608d94cebd7c60a
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-16 18:58:11 +0200

    Added main page

---
+++ app/frontend/webApp/src/pages/homeNotAuth/homeNotAuth.tsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import NavNotAuth from '../../components/navNotAuth/NavNotAuth';
+import Footer from '../../components/footer/Footer';
+import HomeNotAuthMainContent from '../../components/homeNotAuthMainContet/HomeNotAuthMainContent';
+
+export default function HomeNotAuthPage() {
+    return (
+        <>
+            <NavNotAuth />
+            <HomeNotAuthMainContent />
+            <Footer />
+        </>
+    );
+}
