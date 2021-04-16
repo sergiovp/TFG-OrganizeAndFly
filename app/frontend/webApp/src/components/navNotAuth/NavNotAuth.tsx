4d0afa7b65327ae4e04e0c1dfc8485ba89c2044c
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-16 18:52:51 +0200

    Added nav component for not auth users

---
+++ app/frontend/webApp/src/components/navNotAuth/NavNotAuth.tsx
@@ -0,0 +1,40 @@
+import react from 'react';
+import { useHistory } from "react-router-dom";
+import { Navbar, Nav, Row, Col } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
+import logo from '../../public/images/logo.png';
+import './styles.css';
+
+const COMP_NAME = 'NavNotAuth';
+
+export default function NavNotAuth() {
+    let history = useHistory();
+
+    function handleSignUpClick() {
+        history.push('/signup');
+    }
+
+    function handleSignInClick() {
+        history.push('/login');
+    }
+
+    return (
+        <div className={`${COMP_NAME}__main-container`}>
+            <Row>
+            <Navbar bg="light" fixed="top" expand="lg" className={`${COMP_NAME}__nav-container`}>
+                <Navbar.Brand href="/"><img className={`${COMP_NAME}__nav-logo`} src={logo} alt="Logo"/></Navbar.Brand>
+                <Navbar.Brand className={`${COMP_NAME}__nav-tittle`} href="/">Organize&amp;Go</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    </Nav>
+                    <Row>
+                        <Col><Button variant="outlined" color="primary" onClick={handleSignInClick}>LogIn</Button></Col>
+                        <Col><Button variant="outlined" onClick={handleSignUpClick}>Registro</Button></Col>
+                    </Row>
+                </Navbar.Collapse>
+            </Navbar>
+            </Row>
+        </div>
+    );
+}
