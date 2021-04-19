21e2d6e4eba22f5b9d6b1af16f929d918e1c499c
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-19 18:05:32 +0200

    Export knex-session-store instance

--- app/backend/dataBase/instanceDB.ts
+++ app/backend/dataBase/instanceDB.ts
@@ -1,6 +1,13 @@
-import knex from 'knex';
+import Knex from 'knex';
 import knexConfig from '../../../knexfile';
+import knexSessionStore from 'connect-session-knex';
+import session from 'express-session';
 
-const dataBase = knex(knexConfig);
+const knexSession = knexSessionStore(session);
 
-export default dataBase;
+export const knex: any = Knex(knexConfig);
+
+export const storeKnexSession = new knexSession({
+    knex,
+    tablename: 'sessions',
+});
