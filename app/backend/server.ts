3b16f0effd761605bc3e9cefd8d305891867cf51
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-19 18:02:55 +0200

    Added middleware to manage and store in the DB users sessions

--- app/backend/server.ts
+++ app/backend/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/routes';
+import session from 'express-session';
+import { storeKnexSession as store } from './dataBase/instanceDB';
 
 // Instance of our application
 const app = express();
@@ -19,6 +21,16 @@ app.use(express.json());
 
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+// Express-session middleware which also stores the session in the DB
+app.use(
+	session({
+		secret: 'app_secret_key',
+		resave: true,
+		saveUninitialized: true,
+		store,
+	}),
+);
 
 // Charge our routes
 app.use(router);
