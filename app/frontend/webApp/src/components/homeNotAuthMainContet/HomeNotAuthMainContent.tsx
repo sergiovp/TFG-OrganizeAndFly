709b4a1ab787656fc9107a0edec51216aa070dcd
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-21 18:34:56 +0200

    Added home auth main content component

--- app/frontend/webApp/src/components/homeNotAuthMainContet/HomeNotAuthMainContent.tsx
+++ app/frontend/webApp/src/components/homeNotAuthMainContet/HomeNotAuthMainContent.tsx
@@ -11,7 +11,7 @@ export default function HomeNotAuthMainContent() {
             <Row className={`${COMP_NAME}__up-container`}>
                 <Col md="7" className={`${COMP_NAME}__txt-container`}>
                     <h2>Hi, there!</h2>
-                    <h3>Welcome to Organize&amp;Go</h3>
+                    <h3>Welcome to Organize&amp;Fly</h3>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna 
                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
