9d570de338a67cf6ac9fc4655f04067e2adb86ce
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-24 20:03:49 +0200

    Redirect auth user to home in case he wants to go to an no auth page

---
+++ app/frontend/webApp/src/components/unAuthenticatedRoute.tsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import { Route, RouteProps, Redirect } from 'react-router-dom';
+import { useSelector, RootStateOrAny } from 'react-redux';
+
+export default function UnAuthenticatedRoute({component: Component, path}: RouteProps) {
+    const isLogged = useSelector((state: RootStateOrAny) => state.session.isLogged);
+
+    return isLogged ? <Redirect to='/home' /> : <Route component={Component} path={path} />;
+}
