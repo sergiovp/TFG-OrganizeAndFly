4c528ed04ee4b1fd5ceddf65b14462ae2c11ff99
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-24 20:01:04 +0200

    Added save and load middleware to persist store information

--- app/frontend/webApp/src/redux/store.ts
+++ app/frontend/webApp/src/redux/store.ts
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import sessionReducer from './sessionDucks';
+import { save, load } from "redux-localstorage-simple";
 
 const rootReducer = combineReducers({
     session: sessionReducer
 });
 
 export default function generateStore() {
-    const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+    const store = createStore(rootReducer, load(),composeWithDevTools(applyMiddleware(thunk, save())));
     return store;
 }
