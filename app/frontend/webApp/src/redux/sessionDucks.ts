55b7061c1ec79a10de444ed0c03aa73f0e069892
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-24 20:00:21 +0200

    Added tow actions, to create an user session and delete it

--- app/frontend/webApp/src/redux/sessionDucks.ts
+++ app/frontend/webApp/src/redux/sessionDucks.ts
@@ -2,6 +2,7 @@ import { getSession } from '../helpers/auth';
 
 const initialState = {
     user: {
+        token: "",
         id: "",
         email: "",
         isLogged: false,
@@ -9,25 +10,39 @@ const initialState = {
 }
 
 // TYPES
-const CREATE_USER = 'CREATE_USER';
+const CREATE_USER_SESSION = 'CREATE_USER_SESSION';
+const DELETE_USER_SESSION = 'DELETE_USER_SESSION';
 
 export default function sessionReducer(state = initialState, action: any) {
     switch(action.type) {
-        case CREATE_USER:
-            return { ...state, 
-                id: action.payload.user_id,
-                email: action.payload.email,
+        case CREATE_USER_SESSION:
+            return {
+                token: action.payload.token,
+                id: action.payload.userData.user_id,
+                email: action.payload.userData.email,
                 isLogged: true,
+            }
+        case DELETE_USER_SESSION:
+            return {
+                token: "",
+                id: "",
+                email: "",
+                isLogged: false,
             }
         default:
             return state;
     }
 }
 
-export const createUser = () => async (dispath: any, getState: any) => {
-    const res = await getSession();
+export const createUserAction = (sessionData: any) => async (dispath: any, getState: any) => {
     dispath({
-        type: CREATE_USER,
-        payload: res.data,
+        type: CREATE_USER_SESSION,
+        payload: sessionData,
+    });
+}
+
+export const deleteUserAction = () => async (dispath: any, getState: any) => {
+    dispath({
+        type: DELETE_USER_SESSION,
     });
 }
