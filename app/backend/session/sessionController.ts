9df948d1728da05e2a49728f21701f606ab15b13
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-26 20:14:47 +0200

    Deleted the console output

--- app/backend/session/sessionController.ts
+++ app/backend/session/sessionController.ts
@@ -5,6 +5,5 @@ import { getNoSessionDeleted } from '../helpers/errorsFunctions';
 export async function deleteSession(sid: string) {
     const res = await deleteSessionDB(sid);
 
-    res === 1 ? console.log(sessionDeleted()) : console.log(getNoSessionDeleted());
     return res === 1 ? sessionDeleted() : getNoSessionDeleted();
 }
