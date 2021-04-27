d8d4c31d826d6947d4a3d85631e4fa5562f3595a
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-04-27 18:16:34 +0200

    Tabulated Navbar

--- app/frontend/webApp/src/components/navNotAuth/NavNotAuth.tsx
+++ app/frontend/webApp/src/components/navNotAuth/NavNotAuth.tsx
@@ -1,4 +1,3 @@
-import react from 'react';
 import { useHistory } from "react-router-dom";
 import { Navbar, Nav, Row, Col } from 'react-bootstrap';
 import Button from '@material-ui/core/Button';
@@ -21,19 +20,19 @@ export default function NavNotAuth() {
     return (
         <div className={`${COMP_NAME}__main-container`}>
             <Row>
-            <Navbar bg="light" fixed="top" expand="lg" className={`${COMP_NAME}__nav-container`}>
-                <Navbar.Brand href="/"><img className={`${COMP_NAME}__nav-logo`} src={logo} alt="Logo"/></Navbar.Brand>
-                <Navbar.Brand className={`${COMP_NAME}__nav-tittle`} href="/">Organize&amp;Fly</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    </Nav>
-                    <Row>
-                        <Col><Button variant="outlined" color="primary" onClick={handleLogInClick}>LogIn</Button></Col>
-                        <Col><Button variant="outlined" onClick={handleSignUpClick}>SignUp</Button></Col>
-                    </Row>
-                </Navbar.Collapse>
-            </Navbar>
+                <Navbar bg="light" fixed="top" expand="lg" className={`${COMP_NAME}__nav-container`}>
+                    <Navbar.Brand href="/"><img className={`${COMP_NAME}__nav-logo`} src={logo} alt="Logo"/></Navbar.Brand>
+                    <Navbar.Brand className={`${COMP_NAME}__nav-tittle`} href="/">Organize&amp;Fly</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                        </Nav>
+                        <Row>
+                            <Col><Button variant="outlined" color="primary" onClick={handleLogInClick}>LogIn</Button></Col>
+                            <Col><Button variant="outlined" onClick={handleSignUpClick}>SignUp</Button></Col>
+                        </Row>
+                    </Navbar.Collapse>
+                </Navbar>
             </Row>
         </div>
     );
