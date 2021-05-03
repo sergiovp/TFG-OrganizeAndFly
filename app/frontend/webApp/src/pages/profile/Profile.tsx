aa026c89349c1062a3d84c938fa177304b77fddd
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-05-03 19:46:47 +0200

    Implemented profile page

---
+++ app/frontend/webApp/src/pages/profile/Profile.tsx
@@ -0,0 +1,14 @@
+import Footer from '../../components/footer/Footer';
+import NavAuth from '../../components/navAuth/NavAuth';
+import Profile from '../../components/signForms/Profile';
+
+export default function ProfilePage() {
+
+    return (
+        <>
+            <NavAuth />
+            <Profile />
+            <Footer />
+        </>
+    );
+}
