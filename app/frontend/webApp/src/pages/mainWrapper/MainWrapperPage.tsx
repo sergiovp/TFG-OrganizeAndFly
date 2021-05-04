fa400d76086283544f0b644569508343a221a0fb
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-05-04 19:14:01 +0200

    Removed some imports

--- app/frontend/webApp/src/pages/mainWrapper/MainWrapperPage.tsx
+++ app/frontend/webApp/src/pages/mainWrapper/MainWrapperPage.tsx
@@ -1,10 +1,6 @@
-import react, {useEffect, useState} from 'react';
 import Footer from '../../components/footer/Footer';
 import NavAuth from '../../components/navAuth/NavAuth';
 import HomeAuthPage from '../homeAuth/HomeAuth';
-import {  whoAmI } from '../../requests/userRequests'; 
-import { useSelector, RootStateOrAny } from 'react-redux';
-
 
 export default function MainWrapperPage() {
 
