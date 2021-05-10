67feb8d395c565c22ee90b26a8786d5c55a172c2
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-05-10 19:16:21 +0200

    Implementing static HTML

--- app/frontend/webApp/src/pages/homeAuth/HomeAuth.tsx
+++ app/frontend/webApp/src/pages/homeAuth/HomeAuth.tsx
@@ -1,8 +1,25 @@
+import { Row, Container } from 'react-bootstrap';
+import group from '../../public/images/group.png';
+import shared from '../../public/images/shared.png'
+import personal from '../../public/images/personal.png'
+import './style.css';
+
+const COMP_NAME = 'HomeAuth';
 
 export default function HomeAuthPage() {
     return (
         <>
-            <h1>La página principal</h1>
+            <Container className={`${COMP_NAME}__main-container`}>
+                <Row className={`${COMP_NAME}__row-container`}>
+                    <p><img src={personal} alt="icon"/>Personal Boards</p>
+                </Row>
+                <Row className={`${COMP_NAME}__row-container`}>
+                    <p><img src={shared} alt="icon"/>Shared Boards</p>
+                </Row>
+                <Row className={`${COMP_NAME}__row-container`}>
+                    <p><img src={group} alt="icon"/>Teams</p>
+                </Row>
+            </Container>
         </>
     );
 }
