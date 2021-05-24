0873f7f9651ebe396a16729eea9d027ab595729c
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-05-24 20:13:47 +0200

    Mov the server from the app to a separate file

--- app/backend/server.ts
+++ app/backend/server.ts
@@ -1,42 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/routes';
-import session from 'express-session';
-import { storeKnexSession as store } from './dataBase/instanceDB';
-
-// Instance of our application
-const app = express();
+import app from './app';
 
 // Set port
 const PORT = 7777;
-
-// Express middleware to enable CORS with credentials from our frontend
-app.use(cors({
-	origin: 'http://localhost:3000',
-	credentials : true
-}));
-
-// Parse requests of content-type - application/json
-app.use(express.json());
-
-// Parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// Express-session middleware which also stores the session in the DB
-app.use(
-	session({
-		secret: 'app_secret_key',
-		resave: true,
-		saveUninitialized: false,
-		store,
-		cookie: {
-			maxAge: 365 * 24 * 60 * 60 * 1000,
-		}
-	}),
-);
-
-// Charge our routes
-app.use(router);
 
 // Start the server
 app.listen(PORT, () => {
