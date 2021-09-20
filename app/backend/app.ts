f9dd7e8f2a9a809ed8e4b9fff927ba992c6a641d
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-09-20 15:01:54 +0200

    Use env variables

--- app/backend/app.ts
+++ app/backend/app.ts
@@ -1,8 +1,13 @@
-import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
 import router from './routes/routes';
 import session from 'express-session';
+
 import { storeKnexSession as store } from './dataBase/instanceDB';
+
+// Configure DOTENV to use ENV variables.
+dotenv.config();
 
 // Instance of our application
 const app = express();
@@ -22,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 // Express-session middleware which also stores the session in the DB
 app.use(
 	session({
-		secret: 'app_secret_key',
+		secret: process.env.JWT_SECRET || '',
 		resave: true,
 		saveUninitialized: false,
 		store,
