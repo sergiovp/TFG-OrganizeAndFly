7acdc531c8b6de19b6ef717dc73c172947f108e5
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-09-20 15:00:04 +0200

    Add comments and use ENV variables

--- app/backend/middlewares/verifyAuth.ts
+++ app/backend/middlewares/verifyAuth.ts
@@ -1,9 +1,16 @@
 import express, { NextFunction } from 'express';
+import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import { getTokenNotProvided, getTokenNotMatch, getNoSession } from '../helpers/errorsFunctions';
 
-const SECRET = 'app_secret_key';
+// Configure DOTENV to use ENV variables.
+dotenv.config();
 
+const SECRET = process.env.JWT_SECRET || '';
+
+/**
+ * Verify if the user token is valid and match.
+ */
 export function verifyUserToken(req: express.Request, res: express.Response, next: NextFunction) {
     const header = req.headers['authorization'];
 
@@ -21,13 +28,16 @@ export function verifyUserToken(req: express.Request, res: express.Response, nex
                 if (req.session.userToken === token) {
                     return next();
                 } else {
-                    return res.send("No coincide los tokens");
+                    res.status(getTokenNotMatch().status).send(getTokenNotMatch().msg);
                 }
             }
         });
     }
 }
 
+/**
+ * Verify if user session exists.
+ */
 export function verifyUser(req: express.Request, res: express.Response, next: NextFunction) {
     if (req.session && req.session.userToken) {
         return next();
