d211e1bca382c24a96d0f261c0c0bd582218dc50
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-05 20:54:09 +0200

    Implement a default modal

---
+++ app/frontend/webApp/src/components/defaultModal/DefaultModal.tsx
@@ -0,0 +1,33 @@
+import { Modal } from 'react-bootstrap';
+import NameDescriptionForm from '../nameDescriptionForm/NameDescriptionForm';
+
+interface Props {
+    show: boolean;
+    handleClose: any;
+    modalTitle: string;
+    descriptionPlayholder: string;
+    namePlaceholder: string;
+    component: any
+}
+
+export default function DefaultModal(props: Props ) {
+    return (
+        <Modal
+            show={props.show}
+            onHide={props.handleClose}
+            backdrop="static"
+            keyboard={true}
+        >
+            <Modal.Header closeButton>
+            <Modal.Title>{props.modalTitle}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <NameDescriptionForm
+                    descriptionPlaceholder={props.descriptionPlayholder} 
+                    namePlaceholder={props.namePlaceholder}
+                    component={props.component}
+                />
+            </Modal.Body>
+        </Modal>
+    );
+}
