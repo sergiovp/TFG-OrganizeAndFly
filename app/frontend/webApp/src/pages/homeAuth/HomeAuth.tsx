ae3c9d6e23166e13214744364507cc4434a86697
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-05 20:55:58 +0200

    Load modal component when pressing create board or team

--- app/frontend/webApp/src/pages/homeAuth/HomeAuth.tsx
+++ app/frontend/webApp/src/pages/homeAuth/HomeAuth.tsx
@@ -1,17 +1,62 @@
-import { Row, Container } from 'react-bootstrap';
+import { useState } from 'react';
+import { Row, Container, Col, Button } from 'react-bootstrap';
+
+import DefaultModal from '../../components/defaultModal/DefaultModal';
+
 import group from '../../public/images/group.png';
 import shared from '../../public/images/shared.png'
 import personal from '../../public/images/personal.png'
+
 import './style.css';
 
 const COMP_NAME = 'HomeAuth';
 
+enum Component {
+    'BOARD' = 'board',
+    'TEAM' = 'team'
+};
+
 export default function HomeAuthPage() {
+    const [showBoardModal, setShowBoardModal] = useState<boolean>(false);
+    const [showTeamModal, setShowTeamModal] = useState<boolean>(false);
+
+    const handleOnClickBoard = () => {
+        setShowBoardModal(true);
+    };
+
+    const handleOnclickTeam = () => {
+        setShowTeamModal(true);
+    };
+
+    const handleCloseBoardModal = () => {
+        setShowBoardModal(false);
+    }
+
+    const handleCloseTeamModal = () => {
+        setShowTeamModal(false);
+    }
+
     return (
-        <>
-            <Container className={`${COMP_NAME}__main-container`}>
+        <Container className={`${COMP_NAME}__main-container`}>
+            <Col>
                 <Row className={`${COMP_NAME}__row-container`}>
                     <p><img src={personal} alt="icon"/>Personal Boards</p>
+                </Row>
+                <Row className={`${COMP_NAME}__row-dynamic`}>
+                    <Button
+                        variant="primary" 
+                        onClick={handleOnClickBoard}
+                    >
+                        Add new board
+                    </Button>
+                    <DefaultModal 
+                        show={showBoardModal}
+                        handleClose={handleCloseBoardModal}
+                        modalTitle={'Add new board'}
+                        namePlaceholder={'Enter board name'}
+                        descriptionPlayholder={'Enter board description'}
+                        component={Component.BOARD}
+                    />
                 </Row>
                 <Row className={`${COMP_NAME}__row-container`}>
                     <p><img src={shared} alt="icon"/>Shared Boards</p>
@@ -19,7 +64,23 @@ export default function HomeAuthPage() {
                 <Row className={`${COMP_NAME}__row-container`}>
                     <p><img src={group} alt="icon"/>Teams</p>
                 </Row>
-            </Container>
-        </>
+                <Row className={`${COMP_NAME}__row-dynamic`}>
+                    <Button
+                        variant="primary"
+                        onClick={handleOnclickTeam}
+                    >
+                        Add new team
+                    </Button>
+                    <DefaultModal 
+                        show={showTeamModal}
+                        handleClose={handleCloseTeamModal}
+                        modalTitle={'Add new team'}
+                        namePlaceholder={'Enter team name'}
+                        descriptionPlayholder={'Enter team description'}
+                        component={Component.TEAM}
+                    />
+                </Row>
+            </Col>
+        </Container>
     );
 }
