4a099a89740e41b534a5647cb87511dd259ac2b7
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-16 19:21:17 +0200

    Team controller and model

---
+++ app/backend/team/teamController.ts
@@ -0,0 +1,17 @@
+import * as teamModel from './teamModel';
+
+export async function addTeam(teamName: string, teamDescription: string) {
+
+}
+
+export async function setTeam(teamID: string, teamName: string, teamDescription: string) {
+
+}
+
+export async function deleteTeam(teadID: string) {
+
+}
+
+export async function addTeamMember(teamID: string, userEmail: string) {
+
+}
---
+++ app/backend/team/teamModel.ts
@@ -0,0 +1,15 @@
+export async function addTeamDB(teamName: string, teamDescription: string) {
+
+}
+
+export async function setTeamDB(teamID: string, teamName: string, teamDescription: string) {
+
+}
+
+export async function deleteTeamDB(teadID: string) {
+
+}
+
+export async function addTeamMemberDB(teamID: string, userEmail: string) {
+
+}
