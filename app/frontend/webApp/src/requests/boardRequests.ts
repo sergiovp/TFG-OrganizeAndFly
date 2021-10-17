7750f1fecb1025951756650e3c7619cccf6cd3f7
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-17 22:43:44 +0200

    Add a function to get user boards

--- app/frontend/webApp/src/requests/boardRequests.ts
+++ app/frontend/webApp/src/requests/boardRequests.ts
@@ -5,18 +5,32 @@ const URL = 'http://localhost:7777/';
 
 axios.defaults.withCredentials = true;
 
-export async function addBoard(boardName: string, boardDescription: string) {
-    console.log("AQUI");
+export async function addBoard(
+    boardName: string,
+    boardDescription: string,
+    token: string,
+    userID: string) {
+        try {
+            return await axios.post(URL + 'board',
+            {
+                boardName,
+                boardDescription,
+                userID
+            },
+            {
+                headers: generateHeader(token),
+            });
+        } catch (err) {
+            return err.response ? err.response.data : err;
+        }
+}
+
+export async function getUserBoards(token: string, userID: string) {
     try {
-        return await axios.post(URL + 'board',
-        {
-            boardName,
-            boardDescription
-        }/*,
-        {
-            headers: generateHeader('token'),
-        }*/);
-    } catch (err) {
+        return await axios.get(URL + 'board/' + userID, {
+            headers: generateHeader(token),
+        });
+    } catch(err) {
         return err.response ? err.response.data : err;
     }
 }
