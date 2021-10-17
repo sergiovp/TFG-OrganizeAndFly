e46d933c5c72bfde782b0055c3886997e1920876
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-17 17:57:13 +0200

    Upload file for team requests

---
+++ app/frontend/webApp/src/requests/teamRequests.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import generateHeader from '../helpers/generateHeader';
+
+const URL = 'http://localhost:7777/';
+
+axios.defaults.withCredentials = true;
+
+export async function addTeam(teamName: string, teamDescription: string, token: string, userID: string) {
+    try {
+        return await axios.post(URL + 'team',
+        {
+            teamName,
+            teamDescription,
+            userID
+        },
+        {
+            headers: generateHeader(token),
+        });
+    } catch(err) {
+        return err.response ? err.response.data : err;
+    }
+}
+
+export async function getUserTeams(token: string, userID: string) {
+    try {
+        return await axios.get(URL + 'team/' + userID, {
+            headers: generateHeader(token),
+        });
+    } catch(err) {
+        return err.response ? err.response.data : err;
+    }
+}
