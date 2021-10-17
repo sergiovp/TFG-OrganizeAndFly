0fe8ca4512ce9eb95217253d2da25903b4822a16
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-17 22:45:31 +0200

    Add functions to get an user boards

--- app/backend/board/boardModel.ts
+++ app/backend/board/boardModel.ts
@@ -6,22 +6,31 @@ const ID_DB_NAME = 'board_id';
 const BOARD_DB_NAME = 'board_name';
 const BOARD_DB_DESCRIPTION = 'board_description';
 
-export async function addBoardDB(boardID: string, boardName: string, boardDescription: string) {
-    try {
-        await dataBase(TABLE_NAME)
-            .insert({
-                board_id: boardID,
-                board_name: boardName,
-                board_description: boardDescription,
-                board_team: false
-            })
-        return await dataBase(TABLE_NAME)
-            .select('board_id', 'board_name')
-            .first()
-            .where(ID_DB_NAME, boardID);
-    } catch (err) {
-        return getBDError(err);
-    }
+export async function addBoardDB(
+    boardID: string,
+    boardName: string,
+    boardDescription: string,
+    userID: string) {
+        try {
+            await dataBase(TABLE_NAME)
+                .insert({
+                    board_id: boardID,
+                    board_name: boardName,
+                    board_description: boardDescription,
+                    board_team: false
+                })
+            await dataBase('user_board')
+                .insert({
+                    user_id: userID,
+                    board_id: boardID
+                })
+            return await dataBase(TABLE_NAME)
+                .select('board_id', 'board_name')
+                .first()
+                .where(ID_DB_NAME, boardID);
+        } catch (err) {
+            return getBDError(err);
+        }
 }
 
 export async function setBoardDB(boardID: string, boardName?: string, boardDescription?: string) {
@@ -34,4 +43,25 @@ export async function deleteBoardDB(boardID: string) {
 
 export async function shareBoardDB(boardID: string, userEmail: string) {
 
+}
+
+export async function getUserBoardsDB(userID: string) {
+    try {
+        return await dataBase('user_board')
+            .select('board_id')
+            .where('user_id', userID);
+    } catch (err) {
+        return getBDError(err);
+    }
+}
+
+export async function getBoardDB(boardID: string) {
+    try {
+        return await dataBase('boards')
+            .select('board_name', 'board_description')
+            .where('board_id', boardID)
+            .first();
+    } catch (err) {
+        return getBDError(err);
+    }
 }
