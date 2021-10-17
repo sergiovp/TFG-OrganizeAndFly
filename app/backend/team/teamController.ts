e58a444107902da5473cfdb3ea6d59478bb7a6a0
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-10-17 17:55:44 +0200

    Add functions to team controller and model to get user teams

--- app/backend/team/teamController.ts
+++ app/backend/team/teamController.ts
@@ -1,7 +1,11 @@
 import * as teamModel from './teamModel';
+import * as errorFunctions from '../helpers/errorsFunctions';
+import { generateID } from '../helpers/generations';
 
-export async function addTeam(teamName: string, teamDescription: string) {
+export async function addTeam(teamName: string, teamDescription: string, userID: string) {
+    const teamID = generateID();
 
+    const response = await teamModel.addTeamDB(teamID, teamName, teamDescription, userID);
 }
 
 export async function setTeam(teamID: string, teamName: string, teamDescription: string) {
@@ -14,4 +18,15 @@ export async function deleteTeam(teadID: string) {
 
 export async function addTeamMember(teamID: string, userEmail: string) {
 
+}
+
+export async function getUserTeams(userID: string) {
+    const teamsIDs = await teamModel.getUserTeamsDB(userID);
+    let teamInfo = [];
+    
+    for (let team of teamsIDs) {
+        teamInfo.push(await teamModel.getTeam(team.team_id));
+    }
+
+    return teamInfo;
 }
--- app/backend/team/teamModel.ts
+++ app/backend/team/teamModel.ts
@@ -1,5 +1,52 @@
-export async function addTeamDB(teamName: string, teamDescription: string) {
+import { knex as dataBase }  from '../dataBase/instanceDB';
+import { getBDError } from '../helpers/errorsFunctions';
 
+const TABLE_NAME = 'teams';
+const ID_DB_NAME = 'team_id';
+const TEAM_DB_NAME = 'team_name';
+const TEAM_DB_DESCRIPTION = 'team_description';
+
+export async function addTeamDB(teamID: string, teamName: string, teamDescription: string, userID: string) {
+    try {
+        await dataBase(TABLE_NAME)
+            .insert({
+                team_id: teamID,
+                team_name: teamName,
+                team_description: teamDescription,
+            })
+        await dataBase('user_team')
+            .insert({
+                user_id: userID,
+                team_id: teamID
+            })
+        return await dataBase(TABLE_NAME)
+            .select('team_id', 'team_name')
+            .first()
+            .where(ID_DB_NAME, teamID);
+    } catch(err) {
+        return getBDError(err);
+    }
+}
+
+export async function getUserTeamsDB(userID: string) {
+    try {
+        return await dataBase('user_team')
+            .select('team_id')
+            .where('user_id', userID)
+    } catch(err) {
+        return getBDError(err);
+    }
+}
+
+export async function getTeam(teamID: string) {
+    try {
+        return await dataBase('teams')
+            .select('team_name', 'team_description')
+            .where('team_id', teamID)
+            .first()
+    } catch(err) {
+        return getBDError(err);
+    }
 }
 
 export async function setTeamDB(teamID: string, teamName: string, teamDescription: string) {
