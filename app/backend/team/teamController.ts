6e001ed1d944db18f65a94d3a110c5b011747ffb
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-11-07 13:46:21 +0100

    Implement some functions for teams. Advance #21 #22

--- app/backend/team/teamController.ts
+++ app/backend/team/teamController.ts
@@ -1,6 +1,7 @@
 import * as teamModel from './teamModel';
 import * as errorFunctions from '../helpers/errorsFunctions';
 import { generateID } from '../helpers/generations';
+import { getUserEmailByID } from '../user/userModel';
 
 export async function addTeam(teamName: string, teamDescription: string, userID: string) {
     const teamID = generateID();
@@ -9,15 +10,15 @@ export async function addTeam(teamName: string, teamDescription: string, userID:
 }
 
 export async function setTeam(teamID: string, teamName: string, teamDescription: string) {
+    const response = await teamModel.setTeamDB(teamID, teamName, teamDescription);
 
+    return response;
 }
 
-export async function deleteTeam(teadID: string) {
+export async function deleteTeam(teamID: string) {
+    const response = await teamModel.deleteTeamDB(teamID);
 
-}
-
-export async function addTeamMember(teamID: string, userEmail: string) {
-
+    return response;
 }
 
 export async function getUserTeams(userID: string) {
@@ -35,4 +36,28 @@ export async function getTeam(teamID: string) {
     const teamInfo = await teamModel.getTeamDB(teamID);
 
     return teamInfo;
+}
+
+export async function getTeamParticipants(teamID: string) {
+    const usersInfo = await teamModel.getTeamParticipantsDB(teamID);
+    const userEmails = [];
+
+    for (let user of usersInfo) {
+        userEmails.push(await getUserEmailByID(user.user_id));
+    }
+
+    return userEmails;
+}
+
+export async function leaveTeam(userID: string, teamID: string) {
+    const usersInTeam = await teamModel.getTeamParticipantsDB(teamID);
+
+    // There are more users in the team. Just remove this user.
+    if (usersInTeam.length > 1) {
+        return await teamModel.removeUserInTeamDB(userID, teamID);
+    } 
+    // Only this user is in the team. Remove it.
+    else {
+        return await teamModel.deleteTeamDB(teamID);
+    }
 }
