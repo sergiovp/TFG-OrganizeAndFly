4f31cb90dafc797cf7791bc21f55709f365cf455
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-11-07 13:43:58 +0100

    Implement API boards requests. Advance #12 #13 #14 #15 #16 #17

--- app/frontend/webApp/src/requests/boardRequests.ts
+++ app/frontend/webApp/src/requests/boardRequests.ts
@@ -20,7 +20,29 @@ export async function addBoard(
             {
                 headers: generateHeader(token),
             });
-        } catch (err) {
+        } catch (err :any) {
+            return err.response ? err.response.data : err;
+        }
+}
+
+export async function addBoardTeam(
+    boardName: string,
+    boardDescription: string,
+    token: string,
+    teamID: string,
+    userID: string) {
+        try {
+            return await axios.post(URL + 'boardTeam',
+            {
+                boardName,
+                boardDescription,
+                teamID,
+                userID
+            },
+            {
+                headers: generateHeader(token),
+            });
+        } catch (err :any) {
             return err.response ? err.response.data : err;
         }
 }
@@ -30,7 +52,17 @@ export async function getUserBoards(token: string, userID: string) {
         return await axios.get(URL + 'boards/' + userID, {
             headers: generateHeader(token),
         });
-    } catch(err) {
+    } catch(err :any) {
+        return err.response ? err.response.data : err;
+    }
+}
+
+export async function getTeamBoards(token: string, teamID: string) {
+    try {
+        return await axios.get(URL + 'boardsTeam/' + teamID, {
+            headers: generateHeader(token),
+        });
+    } catch(err :any) {
         return err.response ? err.response.data : err;
     }
 }
@@ -40,7 +72,51 @@ export async function getBoard(token: string, boardID: string) {
         return await axios.get(URL + 'board/' + boardID, {
             headers: generateHeader(token),
         });
-    } catch(err) {
+    } catch(err :any) {
+        return err.response ? err.response.data : err;
+    }
+}
+
+export async function deleteBoard(token: string, boardID: string) {
+    try {
+        return await axios.delete(URL + 'board/' + boardID, {
+            headers: generateHeader(token),
+        });
+    } catch (err :any) {
+        return err.response ? err.response.data : err;
+    }
+}
+
+export async function setBoard(token: string, boardID: string, name: string, description: string) {
+    try {
+        return await axios.put(URL + 'board/' + boardID, {
+            name,
+            description
+        },
+        {
+            headers: generateHeader(token),
+        });
+    } catch (err :any) {
+        return err.response ? err.response.data : err;
+    }
+}
+
+export async function isBoardTeam(token: string, boardID: string) {
+    try {
+        return await axios.get(URL + 'isBoardTeam/' + boardID, {
+            headers: generateHeader(token),
+        });
+    } catch(err :any) {
+        return err.response ? err.response.data : err;
+    }
+}
+
+export async function leaveBoard(token: string, userID: string, boardID: string) {
+    try {
+        return await axios.delete(URL + 'leaveBoard/' + userID + '/' + boardID, {
+            headers: generateHeader(token),
+        });
+    } catch (err :any) {
         return err.response ? err.response.data : err;
     }
 }
