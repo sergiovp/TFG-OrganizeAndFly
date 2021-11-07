0c0789450d8fe6993005c211b10051b749b1d19b
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-11-07 13:31:51 +0100

    Implement function to add a board team and leave it. Advance #12 #15

--- app/backend/board/boardController.ts
+++ app/backend/board/boardController.ts
@@ -36,3 +36,38 @@ export async function setBoard(boardID: string, name: string, description: strin
     return response;
 }
 
+export async function addBoardTeam(boardName: string, boardDescription: string, teamID: string, userID: string) {
+    const boardID = generateID();
+
+    return await boardModel.addBoardTeamDB(boardID, boardName, boardDescription, userID, teamID);
+}
+
+export async function getTeamBoards(teamID: string) {
+    const boardsIDs = await boardModel.getTeamBoardsDB(teamID);
+    let boardInfo = [];
+
+    for (let board of boardsIDs) {
+        boardInfo.push(await boardModel.getBoardTeamDB(board.board_id));
+    }
+
+    return boardInfo;
+}
+
+export async function isBoardTeam(boardID: string) {
+    const response = await boardModel.isBoatdTeamDB(boardID);
+
+    return response.board_team;
+}
+
+export async function leaveBoard(userID: string, boardID: string) {
+    const isTeamBoard = await boardModel.getBoardParticipantsDB(boardID);
+
+    // The board has more users. Just delete the actual user.
+    if (isTeamBoard.length > 1) {
+        return await boardModel.deleteParticipant(userID, boardID);
+    } 
+    // This user is the only user in the board. Delete the board.
+    else {
+        return await boardModel.deleteBoardDB(boardID);
+    }
+}
