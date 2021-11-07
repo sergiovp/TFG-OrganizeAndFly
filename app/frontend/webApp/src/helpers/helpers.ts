903cabcabcb16a4005fad66bc811b296f3417d89
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2021-11-07 13:39:11 +0100

    Implement a function to valida an user email

--- app/frontend/webApp/src/helpers/helpers.ts
+++ app/frontend/webApp/src/helpers/helpers.ts
@@ -18,3 +18,8 @@ export function checkEnteredFieldsLogin(email: string, pass: string) {
 export function checkEnteredFieldsSignUp(email: string, pass: string, confirmedPass: string) {
     return email.length > 0 && pass.length > 0 && confirmedPass.length > 0;
 }
+
+export function validateEmail(email: string): boolean {
+    const re: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
